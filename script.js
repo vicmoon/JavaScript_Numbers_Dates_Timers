2f9987803dbc1a28f68fbbc5deb5857357bdd478
Munteanu <munteanuvictoria1@gmail.com>
2024-02-15 16:18:33 +0100

    update

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +(inputLoginPin.value)) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +(inputTransferAmount.value);
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +(inputClosePin.value) === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,74 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+console.log(23 === 23.0);
+// Base 10  from 0 to 9 
+// Binary 2 - 0 and 1  
+console.log(0.1 +  0.2);
+
+//Conversion
+console.log(+('23'));
+console.log(+'23');
+
+//Parsing 
+console.log(Number.parseInt('30px'));
+console.log(Number.parseInt('e23', 10));
+console.log(Number.parseFloat('2.5rem'));
+
+//Check if value is not a value 
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20'));
+console.log(Number.isNaN(23/0));
+
+
+//Checking if a value is a number
+console.log(Number.isFinite('20'));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+
+
+
+console.log(Math.sqrt(25)); //square root
+console.log(8**(1/3)); //cubic root 
+console.log(Math.max(3, 56,'23', 7, 8, 90)); // does type conversion 
+console.log(Math.min(3, 56,'23', 7, 8, 90));
+console.log(Math.PI*Number.parseFloat('10px')**2); // area of a circle with 10px radius 
+console.log(Math.trunc(Math.random() * 10) + 1);  //remove the decimal wiht math.trunc 
+
+const randomInt = (min, max) => Math.floor(Math.random()*(max-min)+1); // 0..1 > 0 ...(max-)
+console.log(randomInt(10, 20));
+
+console.log(Math.trunc(23.4));
+console.log(Math.ceil(23.4)); //24
+console.log(Math.floor(23.4)); //23 
+console.log(Math.trunc(23.4));
+
+// Rounding decimals 
+
+console.log((2.7).toFixed(0)); // will always return a string 
+console.log((2.345).toFixed(2));
+
+
+//Remainder Operator  
+
+console.log(5 % 2); // 
+console.log(5/2); //5 = 2*2 +1 
+
+console.log(8 % 3);
+console.log(8 / 3); 
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(524));
+
+
+labelBalance.addEventListener('click', function() {
+    [...document.querySelectorAll('.movements__row')].forEach(function (row, i){
+    if(i % 2 === 0) row.style.backgroundColor = "blue"
+});
+});
